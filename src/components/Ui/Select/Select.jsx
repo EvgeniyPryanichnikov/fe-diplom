43dbd231e73064ba8901f069f1f293963c16e452
EvgeniyPryanichnikov <pryanichnikov.eugeny@yandex.ru>
2024-06-13 17:25:28 +0400

    travel-form in process

---
+++ src/components/Ui/Select/Select.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import s from './Select.module.css'
+
+const Select = () => {
+  return (
+    <input 
+    className={s.select}
+    type="text" />
+  )
+}
+
+export default Select
--- src/components/common/Travel-form/Travel-form.jsx
+++ src/components/common/Travel-form/Travel-form.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
 
 import s from './Travel-form.module.css'
+import Select from '../../Ui/Select/Select'
 
 const TravelForm = () => {
   return (
-    <div>
-      ФОРМА С ИНПУТАМИ И КНОПКОЙ
+    <div className={s['travel-form']}>
+			<div className={s['travel-form__fields']}>
+				<div className={s['travel-form__direction']}>
+					<p className={s.title}>
+						Направление
+					</p>
+
+					<div className={s['travel-form__direction-fields']}>
+						<Select />
+
+						<Select />
+					</div>
+				</div>
+
+				<div className={s['travel-form__date']}>
+					<p className={s.title}>
+						Дата
+					</p>
+
+					<div className={s['travel-form__date-fields']}>
+						<Select />
+
+						<Select />
+					</div>
+				</div>
+			</div>
+
+			<div className={s['button-container']}>
+				<button className={s['button-find']}>
+					Найти билеты
+				</button>
+			</div>
     </div>
   )
 }
