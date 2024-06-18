50beadf33d33e3486c9c11f2020e17417bcebdc3
EvgeniyPryanichnikov <pryanichnikov.eugeny@yandex.ru>
2024-06-19 00:42:52 +0400

    added steps for tickets-header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 
-// import Main from './pages/Main/Main'
+// import Main from './pages/Main/Main'  ======>>>  Закомментировал чтобы начать делать вторую страницу (<Tickets />).
 import Tickets from './pages/Tickets/Tickets'
 import Footer from './components/global/Footer/Footer'
 
