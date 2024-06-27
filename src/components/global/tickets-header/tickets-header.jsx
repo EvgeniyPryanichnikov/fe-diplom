92a0ea11eef9ef952b3766650bcc0e073f3c79d7
EvgeniyPryanichnikov <pryanichnikov.eugeny@yandex.ru>
2024-06-27 14:29:40 +0400

    rework steps-block

--- src/components/global/tickets-header/tickets-header.jsx
+++ src/components/global/tickets-header/tickets-header.jsx
@@ -2,16 +2,10 @@ import React from 'react'
 
 import s from './Tickets-header.module.scss'
 import Header from '../Header/Header'
-import vector from '../../../icons/vector.png'
+import StepsBlock from './Steps-block/Steps-block'
 import TravelForm from '../../common/Travel-form/Travel-form'
 
 const TicketsHeader = () => {
-  const steps = [
-    {id: 1, text: 'Билеты', needArrow: true},
-    {id: 2, text: 'Пассажиры', needArrow: true},
-    {id: 3, text: 'Оплата', needArrow: true},
-    {id: 4, text: 'Проверка', needArrow: false}
-  ]
   return (
     <div className={s.ticketsHeader}>
       <Header />
@@ -22,27 +16,7 @@ const TicketsHeader = () => {
         />
       </div>
 
-      <div className={s.stepsContainer}>
-        {steps.map(step => (
-          <div
-            className={s.step}
-            key={step.id}
-          >
-            <div className={s.title}>
-              <span className={s.number}>{step.id}</span>
-
-              <p className={s.text}>{step.text}</p>   
-            </div>
-
-            {step.needArrow &&
-              <img
-                className={s.vector}
-                src={vector} alt="" 
-              />
-            }
-          </div>
-        ))}
-      </div>
+      <StepsBlock />
     </div>
   )
 }
